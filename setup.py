5915005ac8476018455e32c89f4088ba300f90f4
Haejoong Lee <haejoong@ldc.upenn.edu>
2014-05-04 20:48:37 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup
 setup(name="at",
       version="1.0.0",
+      description="Collection of widgets for linguistic annotation tools",
+      author="Haejoong Lee",
+      author_email="hajoong@ldc.upenn.edu",
+      url="https://github.com/hleeldc/at",
       packages=['at4'],
       package_dir={'at4':'src'}
       )
